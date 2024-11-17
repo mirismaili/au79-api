a8bec5a486d5ffdffd366b4863a97645234d2eac
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2024-11-17 11:38:47 +0330

    Passkey v0.3

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,12 +1,65 @@
-import {createId} from '@paralleldrive/cuid2'
-import {integer, pgTable, serial, timestamp, varchar} from 'drizzle-orm/pg-core'
+// noinspection JSUnusedGlobalSymbols
+
+import {init as initCuid} from '@paralleldrive/cuid2'
+import {relations} from 'drizzle-orm'
+import {
+  bigint,
+  boolean,
+  char,
+  customType,
+  index,
+  integer,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  uniqueIndex,
+  varchar,
+} from 'drizzle-orm/pg-core'
+
+const createId = initCuid({length: 24})
+
+const bytea = customType<{data: Uint8Array; driverData: Buffer; notNull: false; default: false}>({
+  dataType: () => 'bytea',
+  toDriver: (value) => Buffer.from(value),
+})
 
 export const usersTable = pgTable('user', {
-  id: varchar('id')
+  id: char({length: 24})
     .$defaultFn(() => createId())
     .primaryKey(),
-  username: varchar('username').notNull().unique(),
-  passwordHash: varchar('password_hash', {}).notNull(),
-  email: varchar('email').notNull().unique(),
+  username: varchar({length: 127}).notNull().unique(),
+  passwordHash: varchar('password_hash', {}),
+  phone: bigint({mode: 'number'}).notNull().unique(),
+  email: varchar({length: 255}).notNull().unique(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
+  lastModified: timestamp('last_modified', {mode: 'date'}).defaultNow().notNull(),
 })
+
+/** @see https://simplewebauthn.dev/docs/packages/server#additional-data-structures */
+export const passkeysTable = pgTable(
+  'passkey',
+  {
+    id: text()
+      .$defaultFn(() => createId())
+      .primaryKey(),
+    publicKey: bytea('public_key').notNull(),
+    userId: char('user_id', {length: 24}).references(() => usersTable.id),
+    webauthnUserId: text('webauthn_user_id').notNull(), // A UNIQUE constraint on `webAuthnUserId + userId` also achieves maximum user privacy
+    counter: bigint({mode: 'number'}).notNull(),
+    deviceType: varchar('device_type', {length: 32}).notNull(), // Longest possible value is currently 12 characters // Ex: 'singleDevice' | 'multiDevice'
+    backedUp: boolean('backed_up').notNull(),
+    transports: varchar({length: 255}), // Store string array as a CSV string // Ex: ['ble', 'cable', 'hybrid', 'internal', 'nfc', 'smart-card', 'usb'].join(',')
+  },
+  (table) => [
+    {
+      webauthnUserIdIdx: index().on(table.webauthnUserId),
+      webauthnUserIdAndUserIdIdx: uniqueIndex().on(table.webauthnUserId, table.userId),
+    },
+  ],
+)
+
+export const usersRelations = relations(usersTable, ({many}) => ({passkeys: many(passkeysTable)}))
+export const passkeyRelations = relations(passkeysTable, ({one}) => ({
+  user: one(usersTable, {fields: [passkeysTable.userId], references: [usersTable.id]}),
+}))
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,51 @@
-import {usersTable} from '@/db/schema'
+import * as drizzleSchema from '@/db/schema'
 import {cors} from '@elysiajs/cors'
-import type {VerifiedRegistrationResponse} from '@simplewebauthn/server'
-import {
-  generateRegistrationOptions,
-  verifyRegistrationResponse,
-  VerifyRegistrationResponseOpts,
-} from '@simplewebauthn/server'
-import type {AuthenticatorTransportFuture, RegistrationResponseJSON, WebAuthnCredential} from '@simplewebauthn/types'
+import {swagger} from '@elysiajs/swagger'
+import {generateRegistrationOptions, verifyRegistrationResponse} from '@simplewebauthn/server'
+import type {AuthenticatorTransportFuture, Base64URLString, WebAuthnCredential} from '@simplewebauthn/types'
+import {eq} from 'drizzle-orm'
 import {drizzle} from 'drizzle-orm/node-postgres'
 import {createInsertSchema} from 'drizzle-typebox'
 import {Elysia, t} from 'elysia'
 
-const db = drizzle(Bun.env.DATABASE_URL!)
+const {passkeysTable, usersTable} = drizzleSchema
+const db = drizzle(Bun.env.DATABASE_URL!, {schema: drizzleSchema})
 
-const _createUser = createInsertSchema(usersTable, {
+const insertUserSchema = createInsertSchema(usersTable, {
   email: t.String({format: 'email'}),
 })
-const createUser = t.Intersect([
-  t.Omit(_createUser, ['id', 'createdAt', 'passwordHash']),
-  t.Object({password: t.String()}),
-])
+const usernameSchema = {...t.Index(insertUserSchema, ['username']), minLength: 5}
 
-const credentials: {id: string; transports?: AuthenticatorTransportFuture[]}[] = []
+const tSession = t.Object({
+  challenge: t.String(),
+  username: t.String(),
+  webauthnUserId: t.String(),
+  userId: t.Optional(t.String()),
+})
+
+/** @see https://github.com/sinclairzx81/typebox#unsafe-types */
+const tStringEnum = <T extends string[]>(values: readonly [...T]) => t.Unsafe<T[number]>({...t.String(), enum: values})
+
+const tBase64URLString = t.Unsafe<Base64URLString>(t.String({contentEncoding: 'base64'}))
 
 const app = new Elysia()
+  .use(swagger({path: 'open-api'}))
   .use(cors({origin: 'http://localhost:7900', methods: ['GET', 'POST'], credentials: true}))
   .group('/auth', (app) =>
     app
       .get(
         '/register',
-        async ({cookie: {session}}) => {
+        async ({query: {username}, cookie: {session}}) => {
+          const user = await db.query.usersTable.findFirst({
+            where: eq(usersTable.username, username),
+            with: {passkeys: true},
+          })
+
           const options = await generateRegistrationOptions({
             rpName: 'Au79 API',
             rpID: 'localhost',
-            userName: 'username',
-            timeout: 60000,
+            userName: username,
+            timeout: 60_000,
             attestationType: 'none',
             /**
              * Passing in a user's list of already-registered credential IDs here prevents users from
@@ -42,10 +53,9 @@ const app = new Elysia()
              * error in the browser if it's asked to perform registration when it recognizes one of the
              * credential ID's.
              */
-            excludeCredentials: credentials.map((cred) => ({
-              id: cred.id,
-              type: 'public-key',
-              transports: cred.transports,
+            excludeCredentials: user?.passkeys.map((passkey) => ({
+              id: passkey.id,
+              transports: passkey.transports?.split(',') as AuthenticatorTransportFuture[],
             })),
             authenticatorSelection: {
               residentKey: 'discouraged',
@@ -67,23 +77,39 @@ const app = new Elysia()
            * The server needs to temporarily remember this value for verification, so don't lose it until
            * after you verify the registration response.
            */
-          session.value = {currentChallenge: options.challenge}
+          session.set({
+            sameSite: 'none',
+            secure: true,
+            httpOnly: true,
+            partitioned: true,
+            value: {challenge: options.challenge, username, webauthnUserId: options.user.id},
+          })
 
           return options
         },
         {
-          cookie: t.Cookie({
-            session: t.Optional(t.Object({currentChallenge: t.String()})),
-          }),
+          query: t.Object({username: usernameSchema}),
+          cookie: t.Cookie(
+            {
+              session: t.Optional(t.Partial(tSession)),
+            },
+            // {
+            //   secrets: 'Fischl von Luftschloss Narfidort', // TODO: process.env.COOKIES_SECRET
+            //   sign: ['session'],
+            // },
+          ),
         },
       )
       .post(
         '/register',
         async ({body, cookie: {session}, error}) => {
-          const expectedChallenge = session.value.currentChallenge
+          console.log('body', body)
+          const sessionValue = session.value
+          session.remove()
+          const expectedChallenge = sessionValue.challenge
 
           const {verification, err} = await verifyRegistrationResponse({
-            response: body as RegistrationResponseJSON,
+            response: body,
             expectedChallenge,
             expectedOrigin: 'http://localhost:7900',
             expectedRPID: 'localhost',
@@ -91,7 +117,7 @@ const app = new Elysia()
           })
             .then((verification) => ({verification, err: undefined}))
             .catch((e) => ({verification: undefined, err: e as Error}))
-
+          console.log('verification', verification)
           if (err) {
             console.error(err)
             return error(400, err.message)
@@ -99,33 +125,60 @@ const app = new Elysia()
 
           const {verified, registrationInfo} = verification
 
-          if (verified && registrationInfo) {
-            const {credential} = registrationInfo
+          if (!verified) return error(401, 'Verification failed. Try again.')
 
-            const existingCredential = credentials.find((cred) => cred.id === credential.id)
+          console.log('sessionValue', sessionValue)
+          if (registrationInfo) {
+            let [user] = await db
+              .insert(usersTable)
+              .values({username: sessionValue.username, email: '', phone: 98})
+              .onConflictDoNothing()
+              .returning()
+            user ??= (await db.query.usersTable.findFirst({
+              where: eq(usersTable.username, sessionValue.username),
+              with: {passkeys: true},
+            }))!
 
-            if (!existingCredential) {
-              /**
-               * Add the returned credential to the user's list of credentials
-               */
-              const newCredential: WebAuthnCredential = {
-                id: credential.id,
-                publicKey: credential.publicKey,
-                counter: credential.counter,
-                transports: (body as RegistrationResponseJSON).response.transports,
-              }
-              credentials.push(newCredential)
-            }
+            const {credential, credentialBackedUp, credentialDeviceType} = registrationInfo
+
+            await db
+              .insert(passkeysTable)
+              .values({
+                ...credential,
+                transports: body.response.transports?.join(','),
+                userId: user.id,
+                webauthnUserId: sessionValue.webauthnUserId,
+                backedUp: credentialBackedUp,
+                deviceType: credentialDeviceType,
+              })
+              .onConflictDoNothing()
           }
 
-          session.remove()
-
-          return {verified}
+          return 'Verified' as const
         },
         {
-          cookie: t.Cookie({
-            session: t.Object({currentChallenge: t.String()}),
+          body: t.Object({
+            id: tBase64URLString,
+            rawId: tBase64URLString,
+            response: t.Object({
+              clientDataJSON: tBase64URLString,
+              attestationObject: tBase64URLString,
+              authenticatorData: t.Optional(tBase64URLString),
+              transports: t.Optional(
+                t.Array(tStringEnum(['ble', 'cable', 'hybrid', 'internal', 'nfc', 'smart-card', 'usb'] as const)),
+              ),
+              publicKeyAlgorithm: t.Optional(t.Number()),
+              publicKey: t.Optional(tBase64URLString),
+            }),
+            authenticatorAttachment: tStringEnum(['cross-platform', 'platform'] as const),
+            clientExtensionResults: t.Object({
+              appid: t.Optional(t.Boolean()),
+              credProps: t.Optional(t.Object({rk: t.Optional(t.Boolean())})),
+              hmacCreateSecret: t.Optional(t.Boolean()),
+            }),
+            type: t.Literal<PublicKeyCredentialType>('public-key'),
           }),
+          cookie: t.Cookie({session: tSession}),
         },
       ),
   )
